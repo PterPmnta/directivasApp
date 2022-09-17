b4e7bec638c7fcd49c6717f00fe343f2befaf9f5
pterpmnta <pedro.pimienta.morales@gmail.com>
2022-09-17 14:03:55 -0500

    templateref applied and final section commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CustomIfDirective } from './directives/customIf.directive';
 import { ErrorMsgDirective } from './directives/errorMsg.directive';
 
 @NgModule({
-  declarations: [ErrorMsgDirective],
-  exports: [ErrorMsgDirective],
+  declarations: [ErrorMsgDirective, CustomIfDirective],
+  exports: [ErrorMsgDirective, CustomIfDirective],
 })
 export class SharedModule {}
